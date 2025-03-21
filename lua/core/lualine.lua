e309d249e1eea4465d6781cc0fda6ad9ba908644
H4bbit <Luc4sh4bbit@outlook.com>
2025-03-21 18:36:16 -0300

    lualine fix

--- lua/core/lualine.lua
+++ lua/core/lualine.lua
@@ -1,9 +1,14 @@
 --local symbols = {unix = ''}
 require 'lualine'.setup {
     sections = {
-        lualine_x = { 'filetype' },
+        lualine_b = {'branch', 'diff', 'diagnostics'},
         lualine_c = {
             'lsp_progress'
-        }
+        },
+        lualine_d = { 'progress' },
+        lualine_a = { 'lsp_status' },
+        lualine_x = { 'filetype' },
+        lualine_y = { 'filename' },
+        lualine_z = { 'location' },
     }
 }
