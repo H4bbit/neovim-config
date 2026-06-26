a545998fc713b9ea84ce035e35cbaff6a5005773
H4bbit <luc4sh4bbit@outlook.com>
2026-06-26 18:17:16 -0300

    chore: disable none-ls legacy configuration

--- lua/core/none_ls.lua
+++ lua/core/none_ls.lua
@@ -1,3 +1,4 @@
+--[[
 local null_ls = require("null-ls")
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
@@ -23,9 +24,12 @@ null_ls.setup({
 
 	sources = {
 		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.diagnostics.mypy,
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.diagnostics.ruff,
+		null_ls.builtins.formatting.prettier,
+		--		null_ls.builtins.diagnostics.mypy,
+		--		null_ls.builtins.formatting.black,
+		--		null_ls.builtins.diagnostics.ruff,
 		null_ls.builtins.completion.spell,
 	},
 })
+]]
+--
