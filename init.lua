009448dcf33bf7d5aed15732ea0a2771ae82c772
H4bbit <luc4sh4bbit@outlook.com>
2026-06-28 18:10:45 -0300

    refactor(config): simplify init.lua and clean configuration layout

--- init.lua
+++ init.lua
@@ -1,41 +1,49 @@
 local opt = vim.opt
 
-opt.completeopt = "menuone,noselect"
-opt.backspace = "2"
-
---vim.opt.syntax        = "enable"
+-- Editor
 opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+
+opt.wrap = false
+opt.mouse = "i"
+
 opt.autowrite = true
 opt.autoread = true
 opt.showcmd = true
 
-opt.cursorline = true
-
+-- Indentação
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
-vim.wo.relativenumber = true
+opt.backspace = { "indent", "eol", "start" }
+
+-- Completion
+opt.completeopt = "menuone,noselect"
+
+-- Aparência
+opt.termguicolors = true
+
+-- Leaders
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.cmd([[set nowrap]])
-
-vim.cmd([[set mouse=i]])
-
-vim.opt.termguicolors = true
-
+-- Core
 require("core.plugins")
+
 require("core.lsp")
 require("core.dap")
-require("core.keymaps")
-require("core.wilder")
-require("core.fzf")
 require("core.treesitter")
-require("core.trouble")
-require("core.lualine")
 require("core.none_ls")
 require("core.rust")
 
+require("core.fzf")
+require("core.trouble")
+require("core.lualine")
+
+require("core.keymaps")
+
+-- Plugins sem módulo próprio
 require("colorizer").setup({})
--- lua/core/fzf.lua
+++ lua/core/fzf.lua
@@ -1,27 +1,6 @@
 require("fzf-lua").setup({
-	{ "fzf-native" },
+    { "fzf-native" },
     winopts = {
-	preview_border = "noborder",
+        preview_border = "noborder",
     },
-    --default_previewer = "bat",
 })
-
---[[    winopts = {
-        split = 'belowright new',
-        win_row = 1,
-    },
-    preview_border = 'noborder',
-    preview_vertical = 'down:80%',
-    default_previewer = 'bat',
-    files = {
-        git_icons = false,
-        file_icons = false,
-        color_icons = false,
-    },
-    lsp = {
-        git_icons = false,
-        file_icons = false,
-        color_icons = false,
-    },
-
---]]
