368f77ef9b0c435307bf4fc51344afc4964d8a81
H4bbit <luc4sh4bbit@outlook.com>
2026-07-26 20:34:18 -0300

    feat(lualine): improve statusline and winbar

--- lua/core/lualine.lua
+++ lua/core/lualine.lua
@@ -1,13 +1,39 @@
-require('lualine').setup({
-    options = {
-        theme = 'catppuccin', globalstatus = true,
-    },
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' },
-    }
+require("lualine").setup({
+	options = {
+		theme = "catppuccin",
+		globalstatus = true,
+	},
+
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch", "diff", "diagnostics" },
+		lualine_c = {},
+		lualine_x = { "lsp_progress" },
+		lualine_y = { "progress" },
+		lualine_z = { "location" },
+	},
+
+	winbar = {
+		lualine_a = {
+			{ "filetype", icon_only = true },
+		},
+		lualine_b = {
+			{ "filename", path = 1 },
+		},
+		lualine_c = {},
+		lualine_x = {
+			"diagnostics",
+		},
+		lualine_y = {},
+		lualine_z = {},
+	},
+
+	inactive_winbar = {
+		lualine_a = {
+			{ "filetype", icon_only = true },
+		},
+		lualine_b = {
+			{ "filename", path = 1 },
+		},
+	},
 })
