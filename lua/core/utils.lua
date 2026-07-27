a589c04de0515e4e517c40cf492511d7407c43f2
H4bbit <luc4sh4bbit@outlook.com>
2026-07-27 04:19:20 -0300

    style(lua): format core configuration files

--- lua/core/utils.lua
+++ lua/core/utils.lua
@@ -1,26 +1,26 @@
 local M = {}
 
 function M.map(mode, lhs, rhs, opts)
-    opts = vim.tbl_extend("force", {
-        noremap = true,
-        silent = true,
-    }, opts or {})
+	opts = vim.tbl_extend("force", {
+		noremap = true,
+		silent = true,
+	}, opts or {})
 
-    vim.keymap.set(mode, lhs, rhs, opts)
+	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
 function M.lsp_opts(opts, desc)
-    return vim.tbl_extend("force", opts, {
-        desc = desc,
-    })
+	return vim.tbl_extend("force", opts, {
+		desc = desc,
+	})
 end
 
 function M.augroup(name)
-    return vim.api.nvim_create_augroup(name, { clear = true })
+	return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
 function M.autocmd(event, opts)
-    return vim.api.nvim_create_autocmd(event, opts)
+	return vim.api.nvim_create_autocmd(event, opts)
 end
 
 return M
